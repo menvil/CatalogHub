16d68efe3fbda15aa3643b63037445a99dc4b045
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-08 22:36:43 +0300

    P02-022: Create Dev UI Kit page

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ require __DIR__.'/health.php';
 Route::get('/', function () {
     return view('pages.home');
 });
+
+if (app()->environment(['local', 'testing'])) {
+    Route::get('/dev/ui-kit', function () {
+        return view('dev.ui-kit');
+    })->name('dev.ui-kit');
+}

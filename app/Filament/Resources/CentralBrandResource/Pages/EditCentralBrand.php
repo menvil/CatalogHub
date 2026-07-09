d3b5d9c8687873d43dfcc98efef498cdd89134c1
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-09 13:38:56 +0300

    P03-003: Create Filament resource for brands

---
+++ app/Filament/Resources/CentralBrandResource/Pages/EditCentralBrand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\CentralBrandResource\Pages;
+
+use App\Filament\Resources\CentralBrandResource;
+use Filament\Resources\Pages\EditRecord;
+
+final class EditCentralBrand extends EditRecord
+{
+    protected static string $resource = CentralBrandResource::class;
+}

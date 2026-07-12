0ce6689b619f443d6f8051e41b454c23ea728b93
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-12 14:48:40 +0300

    P08-007: Create MediaAssignment model

---
+++ database/factories/MediaAssignmentFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MediaAsset;
+use App\Models\MediaAssignment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<MediaAssignment>
+ */
+class MediaAssignmentFactory extends Factory
+{
+    protected $model = MediaAssignment::class;
+
+    public function definition(): array
+    {
+        return [
+            'media_asset_id' => MediaAsset::factory(),
+            'entity_type' => 'central_product',
+            'entity_id' => fake()->numberBetween(1, 1000),
+            'role' => fake()->randomElement(['main', 'card', 'gallery', 'hero', 'og']),
+            'position' => 0,
+            'locale' => null,
+            'site_id' => null,
+            'market_id' => null,
+            'is_primary' => false,
+            'visibility' => 'global',
+        ];
+    }
+}

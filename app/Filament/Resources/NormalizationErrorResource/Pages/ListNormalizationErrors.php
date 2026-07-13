1d33af5b63268ddbee847b7ae48698dedf8ee6e5
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-13 15:24:31 +0300

    P09-032: Create Normalization Errors screen

---
+++ app/Filament/Resources/NormalizationErrorResource/Pages/ListNormalizationErrors.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\NormalizationErrorResource\Pages;
+
+use App\Filament\Resources\NormalizationErrorResource;
+use Filament\Resources\Pages\ListRecords;
+
+final class ListNormalizationErrors extends ListRecords
+{
+    protected static string $resource = NormalizationErrorResource::class;
+}

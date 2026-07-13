0c1dfc59dcc086db4c43a47d2f5e2252e154c200
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-13 22:48:45 +0300

    P11-018: Add top_products block

---
+++ resources/views/components/blocks/top-products.blade.php
@@ -0,0 +1,6 @@
+@props(['config' => []])
+
+<section data-theme-block="top_products">
+    <h2>{{ $config['title'] ?? 'Top products' }}</h2>
+    <p>Product projection data will be available in a later phase.</p>
+</section>

af0b2e831fa1f84e11737aff33cb0eb16a7c8497
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-13 15:18:27 +0300

    P09-030: Create Mapping Rules Editor

---
+++ app/Filament/Resources/AttributeMappingResource/Pages/CreateAttributeMapping.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\AttributeMappingResource\Pages;
+
+use App\Filament\Resources\AttributeMappingResource;
+use Filament\Resources\Pages\CreateRecord;
+
+final class CreateAttributeMapping extends CreateRecord
+{
+    protected static string $resource = AttributeMappingResource::class;
+}

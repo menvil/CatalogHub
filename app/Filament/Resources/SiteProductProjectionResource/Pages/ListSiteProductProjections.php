823ae24c379582b313c33d6b7a1668f231ef054a
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-14 13:42:37 +0300

    P12-024: Create Projection Preview screen

---
+++ app/Filament/Resources/SiteProductProjectionResource/Pages/ListSiteProductProjections.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\SiteProductProjectionResource\Pages;
+
+use App\Filament\Resources\SiteProductProjectionResource;
+use Filament\Resources\Pages\ListRecords;
+
+final class ListSiteProductProjections extends ListRecords
+{
+    protected static string $resource = SiteProductProjectionResource::class;
+}

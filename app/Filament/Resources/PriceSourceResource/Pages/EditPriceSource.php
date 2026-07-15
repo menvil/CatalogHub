08d1ea69f9ef8c341218e0e693073ec07e91eee6
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-15 15:26:18 +0300

    P17-029: Create Price Source Admin screen

---
+++ app/Filament/Resources/PriceSourceResource/Pages/EditPriceSource.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\PriceSourceResource\Pages;
+
+use App\Filament\Resources\PriceSourceResource;
+use Filament\Resources\Pages\EditRecord;
+
+final class EditPriceSource extends EditRecord
+{
+    protected static string $resource = PriceSourceResource::class;
+}

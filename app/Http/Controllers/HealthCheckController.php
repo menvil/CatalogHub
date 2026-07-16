14bd6ce565bd3f85a7bf9e9c37e9a7c4fc9d03dc
Ivan Moroz <menvil.menvil@gmail.com>
2026-07-16 22:24:33 +0300

    P21-007: Add application health check

--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -11,6 +11,8 @@ class HealthCheckController extends Controller
         return response()->json([
             'status' => 'ok',
             'app' => 'CatalogHub',
+            'environment' => app()->environment(),
+            'timestamp' => now()->toIso8601String(),
         ]);
     }
 }
